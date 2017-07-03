f8f9951a4b49364a2c7b0493a1e9b6724697c019
LiuXiaoLiang2 <15810140695@163.com>
2017-07-03 22:23:02 +0800

    df

--- Thread-1/src/cn/itcast/demo1/Demo1.java
+++ Thread-1/src/cn/itcast/demo1/Demo1.java
@@ -12,8 +12,8 @@ public class Demo1 {
 		//启动线程,jvm虚拟机就会调用该线程的run()方法
 		//一旦执行start()方法，就会开启这个子线程，就会调用run()方法执行.
 		thread.start();
-		
-		
+		 
+		  
 		
 		//主线程仍然会执行这个main方法
 		for (int i = 0; i < 40; i++) {
