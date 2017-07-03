acd863e3921c710ef341e7b22c42482360a65eaa
LiuXiaoLiang2 <15810140695@163.com>
2017-07-03 22:23:31 +0800

    fsd

--- Thread-1/src/cn/itcast/demo1/Demo2.java
+++ Thread-1/src/cn/itcast/demo1/Demo2.java
@@ -1,7 +1,7 @@
 package cn.itcast.demo1;
 /*
  * 卖票程序
- * 多个窗口同时卖票
+ * 多个窗口同时卖票gggggggggggg
  */
 public class Demo2 {
 
@@ -10,7 +10,7 @@ public class Demo2 {
 		Ticket ticket2 = new Ticket("窗口2");
 		Ticket ticket3 = new Ticket("窗口3");
 		Ticket ticket4 = new Ticket("窗口4");
-		
+		   
 		//开启线程
 		ticket1.start();
 		ticket2.start();
